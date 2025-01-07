7b6ba5ff92f48af6f81db657937f23fd1bff4ad7
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-01-07 15:21:34 +0100

    section 5 (json) : call an api activity - display github informations

--- js-and-react-by-cassidoo-udemy/jsonhandling.js
+++ js-and-react-by-cassidoo-udemy/jsonhandling.js
@@ -1,11 +1,21 @@
-document.getElementsByTagName("button").addEventListener("click", (e) => {
+var response = null;
+// let returns error Uncaught SyntaxError: Identifier 'response' has already been declared
+
+document.getElementsByTagName('button')[0].addEventListener('click', function(r) {
+    getUser(document.getElementsByTagName('input')[0].value);
 });
 
-fetch('https://api.github.com/users/cassidoo')
-.then(function(r) {
-      return r.json();
-  })
-.then(function(j) {
-      console.log(j);
-      document.getElementById('username').innerText = j.login;
-  })
+function getUser(name) {
+    fetch(`https://api.github.com/users/${name}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(j) {
+            console.log('response', j)
+            response = j;
+            document.getElementById('username').innerText = response.login;
+            document.getElementById('name').innerText = response.name;
+            document.getElementById('location').innerText = response.location;
+            document.getElementById('bio').innerText = response.bio;
+        })
+}
