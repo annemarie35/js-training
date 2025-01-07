98b059bd614def0124a157ed2f6b58c3aeda2b22
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-01-07 15:24:06 +0100

    section 5 (json) : call an api activity - fancy it with css

--- js-and-react-by-cassidoo-udemy/jsonhandling.js
+++ js-and-react-by-cassidoo-udemy/jsonhandling.js
@@ -1,21 +1,28 @@
 var response = null;
 // let returns error Uncaught SyntaxError: Identifier 'response' has already been declared
 
-document.getElementsByTagName('button')[0].addEventListener('click', function(r) {
-    getUser(document.getElementsByTagName('input')[0].value);
-});
+document
+    .getElementsByTagName("button")[0]
+    .addEventListener("click", function (r) {
+        getUser(document.getElementsByTagName("input")[0].value);
+    });
+
+function extracted(response) {
+    console.log("response.avatar_url", response.avatar_url);
+    document.getElementById("username").innerText = response.login;
+    document.getElementById("name").innerText = response.name;
+    document.getElementById("location").innerText = response.location;
+    document.getElementById("bio").innerText = response.bio;
+    document.getElementById("avatar").src = response.avatar_url;
+}
 
 function getUser(name) {
     fetch(`https://api.github.com/users/${name}`)
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(j) {
-            console.log('response', j)
+        .then(function (j) {
             response = j;
-            document.getElementById('username').innerText = response.login;
-            document.getElementById('name').innerText = response.name;
-            document.getElementById('location').innerText = response.location;
-            document.getElementById('bio').innerText = response.bio;
-        })
+            extracted(response);
+        });
 }
