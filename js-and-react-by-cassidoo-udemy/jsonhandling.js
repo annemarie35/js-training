f1aa6b0e4fa1060939ae78346c742ebf9702d05c
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-01-07 15:00:26 +0100

    section 5 (json) : call an api activity - add button

---
+++ js-and-react-by-cassidoo-udemy/jsonhandling.js
@@ -0,0 +1,11 @@
+document.getElementsByTagName("button").addEventListener("click", (e) => {
+});
+
+fetch('https://api.github.com/users/cassidoo')
+.then(function(r) {
+      return r.json();
+  })
+.then(function(j) {
+      console.log(j);
+      document.getElementById('username').innerText = j.login;
+  })
