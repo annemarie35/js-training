cd07b031715f326f5c84a9db844299eae5bc9441
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-01-08 17:46:19 +0100

    example javascript bugged

---
+++ exercices.js
@@ -0,0 +1,30 @@
+const menu = [
+    { name: "Margherita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 10 },
+    { name: "Veggie", price: 9 },
+]
+
+const cashInRegister = 100
+const orderQueue = []
+
+function addNewPizza(pizzaObj) {
+    menu.push(pizzaObj)
+}
+
+const placeOrder = (pizzaName) => {
+    const pizzaSelected = menu.find((item) => item.name === pizzaName)
+    if (pizzaSelected) {
+        cashInRegister += pizzaSelected.price
+        orderQueue.push({
+            pizza: pizzaSelected, status: "ordered"
+        })
+    }
+    return orderQueue
+}
+
+function completeOrder(orderId) {
+    const order = orderQueue.find(order => order.id === orderId)
+    order.status = "completed"
+    return order
+}
---
+++ index.md
@@ -0,0 +1,17 @@
+From free code camp
+- https://www.youtube.com/watch?v=SpwzRDUQ1GI
+- https://scrimba.com/learn-typescript-c03c/~00 
+
+autocomplete
+errors compiling
+refactoring
+
+basic, litera and custom types
+optionnal properties
+unions
+type narrowing
+utility types
+generics
+
+Teacher : Bob Ziroll 
+
