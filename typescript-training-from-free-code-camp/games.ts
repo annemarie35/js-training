e542e4f745c84aa3effd875beb45c3d9503030b9
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-01-09 17:13:50 +0100

    generics : use T as convention

--- typescript-training-from-free-code-camp/games.ts
+++ typescript-training-from-free-code-camp/games.ts
@@ -2,6 +2,6 @@ export const gameScores = [14, 21, 33, 42, 59]
 export const favoriteThings = ["raindrops on roses", "whiskers on kittens", "bright copper kettles", "warm woolen mittens"];
 export const voters = [{ name: "Alice", age: 42 }, { name: "Bob", age: 77 }]
 
-export function getLastItem<PlaceholderType>(array: PlaceholderType[]) {
+export function getLastItem<T>(array: T[]) {
     return array[array.length - 1]
 }
