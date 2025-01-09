7d994cf9f34200d57f4992d4798b09cabe38ef43
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-01-09 10:07:53 +0100

    dist folder for compiled ts file

--- free-code-camp/exercices.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var vitest_1 = require("vitest");
-var exercices_1 = require("./exercices");
-(0, vitest_1.test)("Get element that have id with value some-id-value", function () {
-    (0, exercices_1.addNewPizza)({ name: "Napolitan", price: 11 });
-});
