625b23583e968280e4897e8c60bada3f1c744428
Anne-Marie Esteves <annemarie.esteves@yahoo.fr>
2025-11-04 11:17:16 +0100

    chore: install eslint

---
+++ node-snippets/eslint.config.js
@@ -0,0 +1,19 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+
+export default tseslint.config(
+    {
+        ignores: ["node_modules/", "coverage/", "dist/", "eslint.config.js"],
+    },
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintPluginPrettier,
+    {
+        languageOptions: { globals: globals.browser },
+        rules: {
+            "@typescript-eslint/no-unused-vars": "warn",
+        },
+    }
+);
